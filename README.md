be70e7fa8edfac40a63723934b23ab687b9ac330
omnilifedistribuidoraolgadure <151295018+omnilifedistribuidoraolgadure@users.noreply.github.com>
2023-11-27 18:12:50 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# omnilife-web
+ 
---
+++ script.js
@@ -0,0 +1,7 @@
+function showDescription(productId) {
+    document.getElementById(productId).style.display = "block";
+}
+
+function hideDescription(productId) {
+    document.getElementById(productId).style.display = "none";
+}
